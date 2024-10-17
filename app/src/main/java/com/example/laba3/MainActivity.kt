f68af4de4f766fb0905a715bdde8a80d9cc304ae
matatapol22 <matatapol22@gmail.com>
2024-10-17 23:47:49 +0700

    CОЗДАЛИ проект

---
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.laba3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Найдем элементы по их идентификаторам
+
+    }
+}
